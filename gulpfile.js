cb6728492d85c585214c3603e1baaa873d340cf7
Horia Coman <horia141@gmail.com>
2016-10-04 18:19:11 +0300

    Updates to the build process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const gulp = require('gulp');
-const concat = require('gulp-concat');
-const cssnano = require('gulp-cssnano');
-const webpack = require('gulp-webpack');
 const path = require('path');
+const webpack = require('gulp-webpack');
+
 
 gulp.task('index', function() {
     return gulp.src('./src/index.html')
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('js', function() {
+
+gulp.task('js-css', function() {
     return gulp.src('./src/js/app.js')
         .pipe(webpack({
 	    entry: ['./src/js/app.js'],
@@ -18,18 +19,24 @@ gulp.task('js', function() {
 		publicPath: '/dist/',
 		filename: 'app.bundle.js'
 	    },
+	    module: {
+		loaders: [{
+		    test: /\.css$/,
+		    loader: ExtractTextPlugin.extract('style-loader','css-loader')
+		}]
+	    },
+	    plugins: [
+		new ExtractTextPlugin('app.bundle.css')
+	    ],
 	    resolve: {
-		extensions: ['', '.js', '.jsx']
+		extensions: ['', '.js', '.jsx', '.css'],
+		root: [
+		    path.resolve(__dirname, 'src')
+		]
 	    }
 	}))
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('css', function() {
-    return gulp.src('./src/css/**/*.css')
-        .pipe(concat('app.bundle.css'))
-        .pipe(cssnano())
-        .pipe(gulp.dest('./dist'));
-});
 
-gulp.task('build', ['index', 'js', 'css'], function() {});
+gulp.task('build', ['index', 'js-css'], function() {});
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,5 @@
+require('css/app');
+require('bootstrap/dist/css/bootstrap');
+
 document.getElementById('app').innerHTML = 'Hello';
 console.log('Hello world');
